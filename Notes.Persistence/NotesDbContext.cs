81a51370c24c68a4f03dee4484dedc7928fc5adf
IVAN-SHULGA\Winda.by <vanasulga878@gmail.com>
2023-02-04 03:07:55 +0300

    Добавьте файлы проекта.

---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Application.Interfaces;
+using Notes.Persistence.EntityTypeConfigurations;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
